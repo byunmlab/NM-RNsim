7df25b391eb31b84bd4874109de98f7ea6e162e8
jonkb <jonathan.knight.black@gmail.com>
2022-03-16 21:37:26 -0400

    Improve the simple plot script

--- aux_scripts/plot_simple.py
+++ aux_scripts/plot_simple.py
@@ -1,16 +1,29 @@
+import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Set up argparse
+desc = """Scatter plot RMSR against Epoch."""
+parser = argparse.ArgumentParser(description=desc)
+fh = "Provide a csv file to plot"
+parser.add_argument("filename", help=fh)
+parser.add_argument("-o", "--out_file", default="OUT.png",
+  help="Name of output file. Default=OUT.png")
+parser.add_argument("--old", action="store_true",
+  help="Is this an older file format? If so, use the key %%SSR, not %%RMSR")
+args = parser.parse_args()
 
 #ifname = "../../test_results/95_trsts/95_RMSRs_AVG.csv"
 #ofname = "../../test_results/95_trsts/95_RMSRs_AVG.png"
 #ifname = "95_trsts/95_RMSRs_AVG.csv"
 #ofname = "95_trsts/95_RMSRs_AVG.png"
-ifname = "RMSRs_101ABr2.csv"
-ofname = "RMSRs_101ABr2.png"
+#ifname = "RMSRs_101ABr2.csv"
+#ofname = "RMSRs_101ABr2.png"
+ifname = args.filename
+ofname = args.out_file
 
-old = False
-y_key = "%SSR" if old else "%RMSR"
+y_key = "%SSR" if args.old else "%RMSR"
 
 df = pd.read_csv(ifname)
 
